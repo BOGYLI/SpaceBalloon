a073fb14524e54e0d54b7212e81b4f4a278b261f
Nikogenia <info@nikogenia.de>
2024-06-30 20:00:08 +0200

    sensor template

---
+++ template/main.py
@@ -0,0 +1,24 @@
+"""
+Read data from ### sensor
+
+CSV format:
+timestamp, ###
+"""
+
+import time
+import utils
+
+
+logger = utils.init_logger("template")
+
+
+def demo():
+    import random
+    while True:
+        utils.write_csv("template", [random.randint(0, 100)])
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+
+    demo()
