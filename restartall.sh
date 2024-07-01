040d1d983fc67086912411cffd329588b42743dd
spaceballoon <spaceballoon@bodensee-gymnasium.de>
2024-07-01 22:30:17 +0200

    restart all shell script

---
+++ restartall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check if the script is run as root
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "Not running as root"
+    exit
+fi
+
+# Change directory to script directory
+cd "$(dirname "$0")"
+
+# Check for virtual environment
+if [ ! -d ".venv" ]; then
+    echo "Environment not initialized, please run setup.sh first"
+    exit
+fi
+
+# Restart the services
+for service in /etc/systemd/system/balloon-*.service; do
+    echo "Restarting service $(basename "$service")"
+    systemctl restart "$(basename "$service")"
+done
