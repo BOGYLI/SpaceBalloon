a658a704cb6838570d6f9c273f55b5427c727b8d
spaceballoon <spaceballoon@bodensee-gymnasium.de>
2024-07-01 21:01:19 +0200

    run services as root

--- gps/start.sh
+++ gps/start.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Change directory
-cd ~/SpaceBalloon
+# Check if the script is run as root
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "Not running as root"
+    exit
+fi
+
+# Change directory to the parent of the script directory
+cd "$(dirname "$0")"/..
 
 # Set PYTHONPATH
-export PYTHONPATH=~/SpaceBalloon
+export PYTHONPATH="$(dirname "$0")"/..
 
 # Activate the virtual environment
 source .venv/bin/activate
