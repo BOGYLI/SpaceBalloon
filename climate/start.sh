e3e0e8a28cd8e9a8e57d5d9bd999d179cc7f4b4c
spaceballoon <spaceballoon@bodensee-gymnasium.de>
2024-07-03 17:29:17 +0200

    climate module

---
+++ climate/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check if the script is run as root
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "Not running as root"
+    exit
+fi
+
+# Change directory to the parent of the script directory
+cd "$(dirname "$0")"/..
+
+# Set PYTHONPATH
+export PYTHONPATH="$(dirname "$0")"/..
+
+# Activate the virtual environment
+source .venv/bin/activate
+
+# Start python script
+python3 climate/main.py
