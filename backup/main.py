bb483c5495008394027db6f243b7dbf38b916b79
Nikogenia <info@nikogenia.de>
2024-07-17 16:05:00 +0200

    backup module bugfix

--- backup/main.py
+++ backup/main.py
@@ -18,13 +18,13 @@ def main():
         
         # Copy all video files to backup directories that haven't been touched in the last 5 minutes
         for cam in os.listdir(utils.CONFIG["storage"]["video"]["path"]):
-            for video in os.listdir(f"{utils.CONFIG["storage"]["video"]["path"]}/{cam}"):
-                if time.time() - os.path.getmtime(f"{utils.CONFIG["storage"]["video"]["path"]}/{cam}/{video}") > 300:
+            for video in os.listdir(f"{utils.CONFIG['storage']['video']['path']}/{cam}"):
+                if time.time() - os.path.getmtime(f"{utils.CONFIG['storage']['video']['path']}/{cam}/{video}") > 300:
                     for backup in utils.CONFIG["storage"]["video"]["backups"]:
                         if not os.path.exists(f"{backup}/{cam}"):
                             os.makedirs(f"{backup}/{cam}")
                         if not os.path.exists(f"{backup}/{cam}/{video}"):
-                            shutil.copy(f"{utils.CONFIG["storage"]["video"]["path"]}/{cam}/{video}", f"{backup}/{cam}/{video}")
+                            shutil.copy(f"{utils.CONFIG['storage']['video']['path']}/{cam}/{video}", f"{backup}/{cam}/{video}")
                     logger.info(f"Backed up {cam}/{video}")
         time.sleep(60)
 
