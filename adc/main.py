cdd0ec3261b733530043637db93af42b984ed0cb
Nikogenia <info@nikogenia.de>
2024-07-25 20:43:34 +0200

    methane removed

--- adc/main.py
+++ adc/main.py
@@ -28,20 +28,16 @@ def main():
 
     while True:
 
-        methane = ADS.readADC(0) * f
         uv = ADS.readADC(1) * f
         ref3_3 = ADS.readADC(2) * f
 
         uv_adjusted = 3.3 / ref3_3 * uv
         uv_intensity = mapfloat(uv_adjusted, 0.99, 2.8, 0.0, 15.0)
 
-        sensor_resistance = (5.0 - methane) * 10 / methane
-        methane_ppm = math.pow(10, (math.log10(sensor_resistance / 10) - 1.6) / -0.38)
+        logger.info(f"UV: {uv_intensity:.3f}mW/cm²")
 
-        logger.info(f"UV: {uv_intensity:.3f}mW/cm², Methane: {methane_ppm:.3f}ppm")
-
-        utils.write_csv("adc", [uv_intensity, methane_ppm])
-        utils.send_data("adc", {"uv": uv_intensity, "methane": methane_ppm}, logger)
+        utils.write_csv("adc", [uv_intensity])
+        utils.send_data("adc", {"uv": uv_intensity}, logger)
         time.sleep(utils.get_interval("adc"))
 
 
