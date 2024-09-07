e28db48fe49de41984eca58bc39037a8b6260da5
Nikogenia <info@nikogenia.de>
2024-09-07 13:50:57 +0200

    spectral sensor unit

--- spectral/main.py
+++ spectral/main.py
@@ -28,15 +28,18 @@ def main():
         while not sensor.data_ready:
             time.sleep(0.1)
 
+        # Read temperature in Celsius
         temp = sensor.temperature
-        violet = sensor.violet
-        blue = sensor.blue
-        green = sensor.green
-        yellow = sensor.yellow
-        orange = sensor.orange
-        red = sensor.red
 
-        logger.info(f"Temperature: {temp}°C, Violet: {violet:.1f}, Blue: {blue:.1f}, Green: {green:.1f}, Yellow: {yellow:.1f}, Orange: {orange:.1f}, Red: {red:.1f}")
+        # Read spectral data in μW/cm² and convert to mW/cm²
+        violet = sensor.violet / 1000
+        blue = sensor.blue / 1000
+        green = sensor.green / 1000
+        yellow = sensor.yellow / 1000
+        orange = sensor.orange / 1000
+        red = sensor.red / 1000
+
+        logger.info(f"Temperature: {temp:3f}°C, Violet: {violet:.3f}mW/cm², Blue: {blue:.3f}mW/cm², Green: {green:.3f}mW/cm², Yellow: {yellow:.3f}mW/cm², Orange: {orange:.3f}mW/cm², Red: {red:.3f}mW/cm²")
         utils.write_csv("spectral", [temp, violet, blue, green, yellow, orange, red])
         utils.send_data("spectral", {"temp": temp, "violet": violet, "blue": blue, "green": green, "yellow": yellow, "orange": orange, "red": red}, logger)
 
